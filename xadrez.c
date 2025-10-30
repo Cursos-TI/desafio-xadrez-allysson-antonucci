5ce6c560d9040cdeb756bb6be106a8a87786f92f
Allysson <allysson.antonucci.dev@gmail.com>
2025-10-30 14:15:08 +0000

    Desafio Tema 4 - Mestre

--- xadrez.c
+++ xadrez.c
@@ -4,54 +4,75 @@
 // Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
-int main()
-{
-    // ---------- Início do desafio Tema 4: Aventureiro ---------- //
-    
-    // Movimentação da Torre usando a estrutura for (5 casas para a direita):
-    
-    printf("\nMovimentação da Torre:\n");
-    
-    for(int i = 0; i < 5; i++) {
-        printf("Direita\n"); // Imprime a direção do movimento
+#include <stdio.h>
+
+    // ---------- Início do desafio Tema 4: Mestre ---------- //
+
+    // Movimentação da Torre usando recursividade (5x para a direita):
+    void moverTorre(int casas) {
+        if(casas > 0) {
+            printf("Direita\n");
+            moverTorre(casas - 1); // Recursividade
+        }
     }
     
-    // Movimentação do bispo usando a estrutura while (cinco casas na diagonal para cima e à direita):
-    
-    int x = 0;
-    
-    printf("\nMovimentação do Bispo:\n");
-    
-    while(x < 5) {
-        printf("Cima, Direita\n"); // Imprime a direção do movimento
-        x++;
+    // Movimentação da Rainha usando recursividade (8x casas para a esquerda):
+    void moverRainha(int casas) {
+        if(casas > 0) {
+            printf("Esquerda\n");
+            moverRainha(casas - 1); // Recursividade
+        }
     }
     
-    // Movimentação da Rainha usando a estrutura do-while (oito casas para a esquerda):
-    
-    int r = 0; 
-    printf("\nMovimentação da Rainha:\n");
-    
-    do {
-        printf("Esquerda\n");
-        r++;
-    } while (r < 8);
-    
-    // Movimentação do Cavalo usando loops alinhados (duas pra baixo e uma para esquerda):
-    
-    printf("\nMovimentação do Cavalo:\n");
-    
-    int movimentoCavalo = 1;
-    
-    while(movimentoCavalo--) { // Quando movimentoCavalo chega a 0, a condição do while se torna falsa, e o laço é interrompido 
-    
-        for(int i = 0; i < 2; i++){
-            printf("Baixo, ");
+    // Movimentação do cavalo usando loops aninhados (2x para cima e 1x para direita):
+    void moverCavalo() {
+        for(int j = 0; j < 1; j++) {
+            for(int i = 0; i < 2; i++) {
+                printf("Cima, ");
+            }
+            printf("Direita\n");
         }
         
-        printf("Esquerda\n");
     }
+    
+    // Movimentação do bispo usando loops aninhados e recursividade (5x cima, direita):
+    void moverBispo(int casas) {
+        if(casas > 0) {
+            for(int j = 1; j > 0; j--) { // loop mais externo responsável pelo movimento vertical
+                for(int i = 0; i < 1; i++) { // loop mais interno responsável pelo movimento horizontal
+                    printf("Direita, ");
+                }
+            
+                printf("Cima\n");
+                moverBispo(casas - 1); // Recursividade
+            }
+        }
+        
+    }
+    
 
+int main()
+{
+    // Movimento da Torre:
+    printf("\nMovimentação da Torre:\n");
+    moverTorre(5);
+    
+    
+    // Movimento da Rainha:
+    printf("\nMovimentação da Rainha:\n");
+    moverRainha(8);
+    
+    
+    // Movimento do Cavalo:
+    printf("\nMovimentação do Cavalo:\n");
+    moverCavalo();
+    
+    // Movimento do Bispo:
+    printf("\nMovimentação do Bispo:\n");
+    moverBispo(5);
+    
+    
     return 0;
 }
+
 
